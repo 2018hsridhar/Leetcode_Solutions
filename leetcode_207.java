720e777944041e284e70c7a01b2095f1fe3751e8
Hari Sridhar <hari.h.sridhar@gmail.com>
2021-05-22 19:19:06 -0400

    Create leetcode_207.java
    Attempting first topological sort problem / code. Wish me luck!

---
+++ leetcode_207.java
@@ -0,0 +1,25 @@
+/*
+
+URL = https://leetcode.com/problems/course-schedule/
+
+THOUGHT PROCESSES
+
+1. Is a topological sort algorithm - code up topological sort in order to solve the problem
+2. Topological sort is built on DFS
+3. Vertex visited arrays/sets help with having to choose which new vertex to visit 
+4. Since we can have many topological sorts - we can choose any vertex! Order remains arbitrary
+Fill up ordering, in reverse, via recursive call stacks!
+
+
+IDEAL : [T,S] = [O(V+E),O(V)]
+CURRENT : 
+
+Base cases 
+
+Edge Cases
+
+Gotchas
+
+
+
+*/
