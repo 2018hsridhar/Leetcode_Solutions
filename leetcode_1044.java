2518a240afd73100cbf65bf44854211c981f6d74
Hari Sridhar <hari.h.sridhar@gmail.com>
2021-05-24 11:07:50 -0400

    Update leetcode_1044.java

--- leetcode_1044.java
+++ leetcode_1044.java
@@ -15,11 +15,52 @@
     
     First, try the naive hash method for the rolling hash
     Then experiment with a more complicated hash
+    
+    https://leetcode.com/problems/longest-duplicate-substring/
 
  */
 
 class Solution {
-    public String longestDupSubstring(String s) {
-        
+    
+    
+    /*
+    
+        Attempt a solution via the Rabin Karp Substring Algorithm
+    
+    
+     */
+    
+    // used everywhere in program : thus make it a global variable ( not a function variable ) 
+    // Note : current method does not scale to incredibly large prime numbers ( if substrings have a long length too )
+    // This is why the modulus operation might be used later on
+    int prime = 3;
+    
+    public String longestDupSubstring(String s) 
+    {
+        String pattern = "abe";
+        int initHash = computeInitHash(pattern);
+        System.out.printf("For string pattern = %s, initHash = [%d]\n", pattern, initHash);
+        return pattern;
     }
+    
+    
+    // Note : luckily, the string s is composed only of [a-z], so character shifting is much easier
+    public int computeInitHash(String s)
+    {
+        int initHash = 0;
+        int pow = 0;
+        char[] c_arr = s.toCharArray();
+        for(int i = 0; i < c_arr.length; ++i)
+        {
+            int myValue = (c_arr[i] - 'a' + 1);
+            int nextDig = myValue * (int)Math.pow(prime,pow);
+            initHash += nextDig;
+            ++pow;
+        }    
+        return initHash;
+    }
+    
+    
+    
+    
 }
