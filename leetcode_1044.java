4a3776842221918f483cae7b840c7c5ff927c57b
Hari Sridhar <hari.h.sridhar@gmail.com>
2021-05-24 00:02:20 -0400

    Create leetcode_1044.java

---
+++ leetcode_1044.java
@@ -0,0 +1,19 @@
+// 1044. Longest Duplicate Substring
+
+/* 
+    The Rabin-Karp Algorithm is an underlying component to the solution of this problem
+    Attempt to first code up Rabin-Karp
+    
+    Hyperlinks for the Rabin-Karp Algorithm : 
+    1. https://www.youtube.com/watch?v=BQ9E-2umSWc
+    2. https://www.youtube.com/watch?v=oxd_Z1osgCk&t=487s
+    3.
+    
+
+ */
+
+class Solution {
+    public String longestDupSubstring(String s) {
+        
+    }
+}
