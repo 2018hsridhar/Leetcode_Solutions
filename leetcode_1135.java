9e86d3b8be76678209eca6b50eb3dcf289fb5102
Hari Sridhar <hari.h.sridhar@gmail.com>
2021-05-25 00:58:25 -0400

    Create leetcode_1135.java

---
+++ leetcode_1135.java
@@ -0,0 +1,35 @@
+class Solution {
+    
+    /*
+    
+    Code up PRIM'S ALGORITHM HERE IN PLACE OF KRUSHKAL'S ALGORITHM
+    PROBLEM DESCRIPT = 1135. Connecting Cities With Minimum Cost      
+    URL = https://www.youtube.com/watch?v=jsmMtJpPnhU&list=RDCMUCD8yeTczadqdARzQUp29PJw&start_radio=1&rv=jsmMtJpPnhU&t=0
+    
+    */
+    
+    public int minimumCost(int n, int[][] connections) 
+    {
+        
+        
+        
+        
+    }
+    
+    public int lazyPrims(int n, int[][] connections)
+    {
+        int minCost = -1;
+        int edgeCount = 0; // count up to (V-1) edges for termination
+        int s = 0; // denotes the starting node
+        
+        
+        return minCost;
+    }
+        
+    
+    // in future : eagerPrims
+//     public int eagerPrims()
+//     {
+        
+//     }
+}
