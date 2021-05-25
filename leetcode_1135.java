9a748891b822988e593e1f7f224b66d43a3ff750
Hari Sridhar <hari.h.sridhar@gmail.com>
2021-05-25 01:07:54 -0400

    Update leetcode_1135.java

--- leetcode_1135.java
+++ leetcode_1135.java
@@ -3,14 +3,46 @@ class Solution {
     /*
     
     Code up PRIM'S ALGORITHM HERE IN PLACE OF KRUSHKAL'S ALGORITHM
-    PROBLEM DESCRIPT = 1135. Connecting Cities With Minimum Cost      
+    
     URL = https://www.youtube.com/watch?v=jsmMtJpPnhU&list=RDCMUCD8yeTczadqdARzQUp29PJw&start_radio=1&rv=jsmMtJpPnhU&t=0
     
     */
     
+    public class Edge
+    {
+        int src;
+        int dst;
+        int cost;
+        
+        public Edge(int _src, int _dst, int _cost)
+        {
+            this.src = _src;
+            this.dst = _dst;
+            this.cost = _cost;
+        }
+                
+        public String toString()
+        {
+            String s = this.src + ", " + this.dst + ", " + this.cost;
+            return s;
+        }
+    }
+    
+    public class EdgeComparator implements Comparator<Edge>
+    {
+        public int compare(Edge e1, Edge e2)
+        {
+            if(e1.cost < e2.cost)
+                return -1;
+            else if (e1.cost > e1.cost )
+                return 1;
+            return 0;
+        }
+    }
+    
     public int minimumCost(int n, int[][] connections) 
     {
-        
+        return lazyPrims(n,connections);
         
         
         
@@ -22,7 +54,19 @@ class Solution {
         int edgeCount = 0; // count up to (V-1) edges for termination
         int s = 0; // denotes the starting node
         
+        PriorityQueue<Edge> pq = new PriorityQueue<Edge>(new EdgeComparator());
+        for(int i = 0; i < connections.length; ++i)
+        {
+            int[] edge = connections[i];
+            Edge e = new Edge(edge[0],edge[1],edge[2]);
+            pq.add(e);
+        }
         
+        while(pq.size() != 0)
+        {
+            Edge e = pq.remove();
+            System.out.println(e.toString());
+        }
         return minCost;
     }
         
