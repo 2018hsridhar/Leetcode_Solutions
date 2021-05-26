3ad55f1a9cb8a03b6921039311a8ac9dd76b4520
Hari Sridhar <hari.h.sridhar@gmail.com>
2021-05-26 00:58:28 -0400

    Update leetcode_587.java

--- leetcode_587.java
+++ leetcode_587.java
@@ -13,7 +13,10 @@
     Intuition : tauting a string about a set of points on a 2D-plane
     
     Any while loop requires a breakpoint : otherwise, can not escsape out of infinite iteration
-
+    If all points are collinear, the convex hull results in a straight line
+    
+    Base cases of convex hull : a trianle or two points on a line
+    
     THOUGHT PROCSESES 
 */
 
@@ -47,37 +50,76 @@ class Solution {
         
         
         // We know the hull bound is the length of the trees, but still use an array list here
-        ArrayList<int[]> jarvisMarchPoints = new ArrayList<int[]>();
+        // Remember : it is a 2-point algorithm here
+        // Better : pick the second initially point more randomly ( once Left most point has been established here )
         
+        // Pick a second random point, index not the first random point
+        int[] secondPoint = trees[0];
+        for(int i = 1; i < trees.length; ++i )
+        {
+            int[] myPoint = trees[i];
+            if(equalPoints(curPoint, leftMostPoint) == false)
+                secondPoint = myPoint;
+        }
+        
+        ArrayList<int[]> jarvisMarchPoints = new ArrayList<int[]>();
+        TreeSet<Integer> collinearPoints = new TreeSet<Integer>();
         int[][] results = new int[trees.length][2];
+        int[] curPoint = leftMostPoint; // the starting point
         while(true)
         {
-            int[] initPoint = leftMostPoint;
-            int[] nextLMP;
-            for(int i = 1; i < trees.length; ++i)
+            int[] nextLMP = new int[2];
+            for(int i = 0; i < trees.length; ++i)
             {
                 nextLMP = trees[i];
-                int[] curPoint = trees[i]
-                int crossProd = crossProduct(precPoint, curPoint, nextLMP);
+                int crossProd = crossProduct(curPoint, secondPoint, nextLMP);
                 if(crossProd < 0)
                 {
-                    jarvisMarchPoints.add(nextLMP);
-                    // Have a match here!
+                    secondPoint = nextLMP;
                 }
                 else if ( crossProd == 0 )
                 {
-                    
+                    // check colinear points set
+                    for(int j = 0; j < collinearPoints.size(); ++j)
+                    {
+                        int[] myColPoint = collinearPoints[j];
+                        if(distance(curPoint, secondPoint, nextLMP) == 1)
+                        {
+                            collinearPoints.add(nextLMP); // both values are collinear                        
+                        }
+                        {
+                        else // -1 case : second point/midpoint remains closer
+                        {
+                            collinearPoints.add(secondPoint);
+                        }
+                    }
                 }
                 else
                     continue; // point is not to the left of the current axis
+
+            }
                 
+                                
+            // ADD ALL COLLINEAR POINTS AT END OF EACH ITERATION
+            if(collinearPoints.size() > 0)
+            {
+                Iterator<Integer> it = collinearPoints.iterator();
+                while(it.hasNext())
+                    jarvisMarchPoints.add(collinearPoints.next());
+                }
+                collinearPoints.clear();
             }
             
+            // Check if nextLMP = leftMostPoint here
             if(equalPoints(nextLMP, leftMostPoint))
                 break;
             
-            initPoint = nextLMP; // continue algorithm
+            curPoint = nextLMP; // continue algorithm
         }
+    
+        // Broke from while loop since nextLMP = leftMostPoint
+        // unlike the regular JMP algorithm, do not add back the original tree here!
+        // jarvisMarchPoints.add(leftMostPoint);
         
         for(int i = 0; i < jarvisMarchPoints.size(); ++i)
         {
@@ -99,14 +141,14 @@ class Solution {
     
     // check which point is closer to point a : {b,c}
     // return the point
-    public int[] distance(int[] a, int[] b, int[] c)
+    public int distance(int[] a, int[] b, int[] c)
     {
         double dist_ab = Math.sqrt(Math.pow(a[0] - b[0],2) + Math.pow(a[1] - c[1],2));
         double dist_ac = Math.sqrt(Math.pow(a[0] - c[0],2) + Math.pow(a[1] - c[1],2));
         
         if(dist_ac >= dist_ab )
-            return c;
-        return b;
+            return 1;
+        return -1;
     }
     
     public boolean equalPoints(int[] p1, int[] p2)
