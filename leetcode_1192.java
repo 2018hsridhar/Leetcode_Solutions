6b7276471730f02a0e0c9b041706565a787c8f5b
Hari Sridhar <hari.h.sridhar@gmail.com>
2021-05-27 00:41:52 -0400

    Update leetcode_1192.java

--- leetcode_1192.java
+++ leetcode_1192.java
@@ -8,6 +8,11 @@ Links/Resources to consult upon :
 3.https://www.geeksforgeeks.org/detect-cycle-undirected-graph/#:~:text=%20Depth%20First%20Traversal%20can%20be%20used%20to,its%20ancestor%20in%20the%20tree%20produced%20by%20DFS.
 4. https://www.geeksforgeeks.org/detect-cycle-in-a-graph/
 
+
+https://codeforces.com/problemset/problem/117/C
+https://codeforces.com/problemset/problem/232/A
+https://codeforces.com/problemset/problem/263/D
+
 Watch full videos, and code up (3) and (4) first
 Understand that intuition, and then proceed with coding up Tarjan's or Kosaraju's!
 
