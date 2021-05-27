1430777c1cab32c4652bad9f79e3454890bb99fd
Hari Sridhar <hari.h.sridhar@gmail.com>
2021-05-27 00:26:22 -0400

    Create leetcode_1192.java

---
+++ leetcode_1192.java
@@ -0,0 +1,21 @@
+
+/*
+
+Links/Resources to consult upon : 
+
+1. https://www.youtube.com/watch?v=RYaakWv5m6o
+2. https://www.youtube.com/watch?v=wUgWX0nc4NY&t=585s
+3.https://www.geeksforgeeks.org/detect-cycle-undirected-graph/#:~:text=%20Depth%20First%20Traversal%20can%20be%20used%20to,its%20ancestor%20in%20the%20tree%20produced%20by%20DFS.
+4. https://www.geeksforgeeks.org/detect-cycle-in-a-graph/
+
+Watch full videos, and code up (3) and (4) first
+Understand that intuition, and then proceed with coding up Tarjan's or Kosaraju's!
+
+*/
+
+
+class Solution {
+    public List<List<Integer>> criticalConnections(int n, List<List<Integer>> connections) {
+        
+    }
+}
