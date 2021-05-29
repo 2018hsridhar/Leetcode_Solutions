124813372c367bbeb7be44f998e4f6ecaae36e0a
Hari Sridhar <hari.h.sridhar@gmail.com>
2021-05-28 23:58:40 -0400

    Create leetcode_211.java

---
+++ leetcode_211.java
@@ -0,0 +1,42 @@
+/*
+
+    Notice how a max to the number of calls ir provided as a potential input too
+    Alphabet range : [a-z] = 26 children max = max number of characters ( or the . character )
+    Meant to match wildcard matching ( as performed by UNIX tools ) 
+    
+    Problem Statement = 211. Design Add and Search Words Data Structure
+    url = https://leetcode.com/problems/design-add-and-search-words-data-structure/
+
+    
+
+ */
+ 
+ class WordDictionary {
+
+    /** Initialize your data structure here. */
+    public WordDictionary() {
+        
+    }
+    
+    public void addWord(String word) 
+    {
+        
+    }
+    
+    // Not as trivial as the other problem - the [.] can indicate matching ANY word!
+    // Must iterate over all of those search strings then!
+    
+    public boolean search(String word) 
+    {
+        
+    }
+}
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary obj = new WordDictionary();
+ * obj.addWord(word);
+ * boolean param_2 = obj.search(word);
+ */
+
+
