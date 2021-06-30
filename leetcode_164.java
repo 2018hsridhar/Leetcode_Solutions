d0b298f5df38c1095a2394defe08f7a4099f05a1
Hari Sridhar <hari.h.sridhar@gmail.com>
2021-06-30 11:47:09 -0400

    Create leetcode_164.java

---
+++ leetcode_164.java
@@ -0,0 +1,44 @@
+
+
+/*
+
+164. Maximum Gap
+URL = https://leetcode.com/problems/maximum-gap/
+
+THOUGHT PROCESS : 
+Let us test our knowledge of the radix sort algorithm
+Things I will ask you in a sort algorithm : 
+1. What are your keys used here?
+2. How to handle case of shared keys? Is your sort algorithm stable or unstable?
+3. What subroutine are you using?
+4. Can we establish the depth of a sorting algorithm ( e.g. Radix / Merge ) . If recursive, note time and space complexity of each step
+5. Is the sorting algorithm in-place or uses external memory?
+6. Can we establish a type of convergence?
+7. Is it a comparitive ( e.g. merge/quick ) or a non-comparative sort algorithm?
+8. Talk about sorting algorithms in terms of keys ( not elements ) : sometimes, we compute some info from each element.
+
+
+
+EDGE CASES : 
+Singleton array case -> return 0
+
+
+DATA TYPE LIMITS : 
+Length of array reasonably guranteed from 1,2,....100,000
+Elements of array reasonably guaranteed from 1,2,...,1,000,000,000
+
+
+
+
+Edge cases : 
+*/
+
+class Solution 
+{
+    public int maximumGap(int[] nums) 
+    {
+        int maxGap = Integer.MIN_VALUE;
+        
+        return maxGap;    
+    }
+}
