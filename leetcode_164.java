6593757d8a1e3d14bf31b46017eeeb8703ebed3d
Hari Sridhar <hari.h.sridhar@gmail.com>
2021-07-01 20:01:22 -0400

    Update leetcode_164.java

--- leetcode_164.java
+++ leetcode_164.java
@@ -1,3 +1,4 @@
+```
 // Accompanying hyperlinks 
 
 // Radix Sorting : 
@@ -26,11 +27,8 @@ Things I will ask you in a sort algorithm :
 6. Can we establish a type of convergence?
 7. Is it a comparitive ( e.g. merge/quick ) or a non-comparative sort algorithm?
 8. Talk about sorting algorithms in terms of keys ( not elements ) : sometimes, we compute some info from each element.
-9. Is a lexicographic sort : works across multiple data dimensions ( x1,x2,...,xn ). Sort from the broadest dimension unto the narrowest dimension. Max number of digits can be considered a "dimension" for our data! Most familiar lexicographic orderings are 2D cartesian planes or dictionary orderings.
+9. Is a lexicographic sort : works across the dimensions of tuples of data ( x1,x2,...,xn ). Sort from the broadest dimension unto the narrowest dimension. Max number of digits can be considered a "dimension" for our data! Most familiar lexicographic orderings are 2D cartesian planes or dictionary orderings.
 10. Want to see if interviee understands either "bucketsort" or "countsort"
-
-
-Use the explanation of count sort from wikipedia, which works off of keys of the input array -> not directly elements of the input array
 
 
 EDGE CASES : 
@@ -42,7 +40,15 @@ Length of array reasonably guranteed from 1,2,....100,000
 Elements of array reasonably guaranteed from 1,2,...,1,000,000,000
 
 
+Dividing by 10 is akin to a bit shift in zeroes place
+E.g. ( 7530/10  = 750 ) &&& ( 750 / 10  )= 75.
+Take 7530 : we will go 7530 -> 753 -> 75 -> 7 over each round
+And each modulo in the steps yields us 0 -> 3 -> 5 -> 7 ( modulo by 10 ).
+Imagine it being like (7500>>1) in base 10
+Now can we use (>>1) for counting number of binary digits?? 
+We can not really prepend leading zeroes here, sadly!
 
+Modulo 10 = (x && 1 ) too
 
 Edge cases : 
 */
@@ -53,28 +59,56 @@ class Solution
     {
         int maxGap = Integer.MIN_VALUE;
         if(nums.length == 1) return 0;
-        // Arrays.sort(nums);
-        // int maxDigit = findMaxNumDigits(nums);
-        // for(int i = 0; i < maxDigit; ++i)
-        // {
-            // execteCountingSort(nums,i);
-        // }
-        // System.out.printf("Number digits max = %d\n", findMaxDigit(nums));
-        
+        int maxDigit = findMaxNumDigits(nums);
+        for(int i = 1; i <= maxDigit; ++i)
+            executeCountSort(nums,i);
         for(int i = 0; i < nums.length - 1; ++i)
             maxGap = Math.max(maxGap, nums[i+1]-nums[i]);
         return maxGap;    
     }
     
-    public int findMaxDigit(int[] nums)
+    public void executeCountSort(int[] nums, int depth)
     {
-        int maxDigLen = Integer.MIN_VALUE;
-        for(int x : nums )
-            maxDigLen = Math.max(maxDigLen, countNumOfDigits(x));
-        return maxDigLen;
+        // [1] Iterate over array, and solve for each digit representation
+        // Convert to own method in production code possibly?
+        int[] results = new int[nums.length];
+        int divisor = (int)Math.pow(10,depth-1);
+
+        // [2] Allocate auxillary space for prefix sum couting
+        // Guaranteed to be an array of length = 10
+        // Generate the prefix array count
+        int[] psa = new int[10];
+        for(int i = 0; i < nums.length; ++i)
+        {
+            int key = ( nums[i] / divisor ) % 10;
+            psa[key] += 1;
+        }
+        
+        // [3] Perform prefix array portion of algorithm
+        for(int i = 1; i < psa.length; ++i)
+            psa[i] = psa[i] + psa[i-1];
+        
+        // [3] Iterate over initial range of values ( nums ) backwards and decremnt prefix array counts
+        for(int i = nums.length - 1; i >= 0; --i)
+        {
+            int key = ( nums[i] / divisor) % 10;
+            --psa[key];
+            results[psa[key]] = nums[i];
+        }        
+        
+        for(int i = 0; i < results.length; ++i)
+            nums[i] = results[i];
     }
     
-    public int countNumOfDigits(int x)
+    public int findMaxNumDigits(int[] nums)
+    {
+        int numDig = Integer.MIN_VALUE;
+        for(int el : nums)
+            numDig = Math.max(numDig, countNumDigits(el));
+        return numDig;        
+    }
+    
+    public int countNumDigits(int x)
     {
         int count = 0;
         while(x >= 10)
@@ -82,17 +116,9 @@ class Solution
             x /= 10;
             ++count;
         }
-        ++count;
+        count += 1;
         return count;
     }
-    
-    // Modulus by 10 at the end here
-    public void executeCountingSort(int[] nums, int pow)
-    {
-        int divisor = Math.pow(10,pow);
-        
-        
-    }
-    
-    
+     
 }
+```
