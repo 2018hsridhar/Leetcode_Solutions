96d85c895e10a927f0d84be0420cc679f9b60299
Hari Sridhar <hari.h.sridhar@gmail.com>
2021-07-09 22:00:16 -0400

    Update leetcode_622.java

--- leetcode_622.java
+++ leetcode_622.java
@@ -34,6 +34,17 @@ Operations still FIFO based
 
 Problem makes sense after consideration of the two pointers technique
 
+EDGE CASE TESTING : 
+(1) Singleton size queue
+- enqueue(x) should not work twice
+- deQueue() should also not work twice
+sz = 1 ; idx = 0 ; (o + 1 ) % 1 = 0 => case fails quickly
+
+
+(2) Queue of k elememts, not fully filled
+
+
+(3) Queue of k elements, more than fully filled
 
 */
 
@@ -61,10 +72,26 @@ class MyCircularQueue
     // One ptr to use consistently here only!
     public boolean enQueue(int value) 
     {
+        // SINGLETON QUEUE CASE
+        if(size == 1)
+        {
+            if(elements[0] == -1)
+            {
+                elements[0] = value;
+                return true;
+            }
+            else
+                return false; // already filled : do not try out here
+        }
+        
+        
+        // NON-SINGLETON QUEUE CASE
         // Check if rear ptr can be incremented
         rearPtr = (( rearPtr + 1 ) % size );
         if(elements[rearPtr] != -1)
             return false; // a value already exists here!
+        if(rearPtr == frontPtr)
+            return false; // already incremented : clearly equals here!
         
         elements[rearPtr] = value;
         return true;
@@ -83,16 +110,19 @@ class MyCircularQueue
         elements[frontPtr] = -1;
         frontPtr = (( frontPtr - 1 ) % size );
         return true; 
-        
     }
     
     public int Front() 
     {
+        if(elements[frontPtr] == -1)
+            return -1;
         return elements[frontPtr];
     }
     
     public int Rear()
     {
+        if(elements[rearPtr] == -1)
+            return -1;
         return elements[rearPtr];
     }
     
