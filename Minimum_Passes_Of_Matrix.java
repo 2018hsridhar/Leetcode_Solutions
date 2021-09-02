f13bcf6410384cd7176b324cea19d3383b9ac666
Hari Sridhar <hari.h.sridhar@gmail.com>
2021-09-01 22:53:42 -0400

    Update Minimum_Passes_Of_Matrix.java

--- Minimum_Passes_Of_Matrix.java
+++ Minimum_Passes_Of_Matrix.java
@@ -10,11 +10,24 @@ class Program {
 	Input matrix always guaranteed one element
 	Return {-1} if negative ints CAN NOT be converted to positives
 	Seems recursive in nature now : akin to Uber - > multiple starting nodes can start the BFS level-order traversal ( many root nodes problem ) 
+	ADJACENT ELEMENT => Cardinal directions restricted { N,S,E,W } 
 	
+	But how to pass level information too?
+
+	Let us suppose that (m,n) can get very huge, BUT, fit in-memory
+	Can we check if we even have a decrement or not here ( over the queue ) ?
 	
 	COMPLEXITY : 
 	TIME = O(MN)
 	SPACE = O(1)
+	
+	Using a queue for recursion : 
+	WORST CASE TIME-SPACE
+	TIME = O((MN)^2)
+	SPACE = O(MN), supposing each element is a negative value
+	
+	Think : in a sense, the depth of a queue, OR, the depth of a stack, grows only so large too!
+	
 	
 	*/
   public int minimumPassesOfMatrix(int[][] matrix) {
