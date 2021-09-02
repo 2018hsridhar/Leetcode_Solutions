4cdfdde652915db2ae30e853278c0101d0e8c563
Hari Sridhar <hari.h.sridhar@gmail.com>
2021-09-01 22:09:19 -0400

    Create Minimum_Passes_Of_Matrix.java

---
+++ Minimum_Passes_Of_Matrix.java
@@ -0,0 +1,24 @@
+// https://www.algoexpert.io/questions/Minimum%20Passes%20Of%20Matrix
+
+import java.util.*;
+
+class Program {
+
+	/*
+	Seems to be a binary search algorithm for sure
+	Also seems to be a level-order BFS too ( utilize queue iteration ) 
+	Input matrix always guaranteed one element
+	Return {-1} if negative ints CAN NOT be converted to positives
+	Seems recursive in nature now : akin to Uber - > multiple starting nodes can start the BFS level-order traversal ( many root nodes problem ) 
+	
+	
+	COMPLEXITY : 
+	TIME = O(MN)
+	SPACE = O(1)
+	
+	*/
+  public int minimumPassesOfMatrix(int[][] matrix) {
+    // Write your code here.
+    return -1;
+  }
+}
