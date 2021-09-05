2cba927a3a27af5c7f8fc71cf78a4142e9ba1392
Hari Sridhar <hari.h.sridhar@gmail.com>
2021-09-05 12:03:49 -0400

    Create leetcode_1466.java

---
+++ leetcode_1466.java
@@ -0,0 +1,19 @@
+/*
+
+THOUGHT PROCESS : 
+n cities at maximum here NUMBERED [0,n-1]
+1466. Reorder Routes to Make All Paths Lead to the City Zero
+URL = https://leetcode.com/problems/reorder-routes-to-make-all-paths-lead-to-the-city-zero/
+
+Input matrix is an edge list ( n x 2 ) ; not a (nxk, where k in 0..n-1) here
+Return number of edges inverted/changed here
+Network guaranteed a tree structure too ( n-1 roads only ) =? only one way to travel betweeen cities
+
+*/
+class Solution
+{
+    public int minReorder(int n, int[][] connections) 
+    {
+        
+    }
+}
