dbbef1c456a6947e49b2e1e06f5093a1f8de64d5
Hari Sridhar <hari.h.sridhar@gmail.com>
2021-09-05 20:13:03 -0500

    Create leetcode_1319.java

---
+++ leetcode_1319.java
@@ -0,0 +1,45 @@
+/*
+ATTEMPT THE UNION-FIND SOLUTION AGAIN HERE
+URL = https://leetcode.com/problems/number-of-operations-to-make-network-connected/
+1319. Number of Operations to Make Network Connected
+Return min times needed to extract specific cables and place between disconnected cables to connect the entire network
+If (#_extra_cables)<(#_seperate_connected_components), return -1
+
+As UNION_FIND tends to be it's own data structure, we want to write up a class
+Easiest to represent via an array too
+Also easy to obtain root of a node too
+
+
+COMPLEXITY : 
+
+
+TEST CASES : 
+
+
+Limitations of the Union-Find Algorithm
+(1) Does NOT work without in-memory storage
+(2) Does NOT work if we do NOT know the total number of nodes to perform operation on
+
+*/
+
+class UnionFind
+{
+    
+}
+
+class Solution 
+{
+    public int makeConnected(int n, int[][] connections)
+    {
+        int num_extra_cables = 0;
+        int num_conn_comps = 0;
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+}
