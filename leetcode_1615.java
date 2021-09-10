7ece61a757f9c57775a4565d6773bcb76bfab925
Hari Sridhar <hari.h.sridhar@gmail.com>
2021-09-10 14:46:46 -0500

    Create leetcode_1615.java

---
+++ leetcode_1615.java
@@ -0,0 +1,12 @@
+/*
+1615. Maximal Network Rank
+URL = https://leetcode.com/problems/maximal-network-rank/
+
+*/
+class Solution
+{
+    public int maximalNetworkRank(int n, int[][] roads) 
+    {
+        
+    }
+}
