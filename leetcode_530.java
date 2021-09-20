1e9416eb225055a1b3c6880583ccefed7bfa903d
Hari Sridhar <hari.h.sridhar@gmail.com>
2021-09-19 21:18:25 -0400

    Update leetcode_530.java

--- leetcode_530.java
+++ leetcode_530.java
@@ -40,20 +40,83 @@ In singleton case, ( min = 0, max = 0 ) of the null children!
 Min absolute difference = 0 : will tank you in the case of the leaf node
 
 */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+/*
+
+Num nodes bounded anyways from [2,10,000]
+Node values strictly bounded by [0,100,000]
+
+URL = https://leetcode.com/problems/minimum-absolute-difference-in-bst/
+530. Minimum Absolute Difference in BST
+
+It is any two nodes -> so efficiency is only so crucial too
+We can be quuick and get the fist minimum
+Can also sort the input and return the difference of first two elements in our BST
+
+BST :- Binary Search Tree
+
+It is a BST, so we know the difference of nodes quickly -> but do not just presume it is a left path reading problem
+    
+    
+Example_1 ::
+
+        4
+       / \
+      2   6
+     / \
+    0   3
+    
+RETURN |3-2| = 1
+
+Example_2 ::
+
+          8
+        /   \
+      6      14
+     / \    /
+    2   7  11
+            \
+             12
+
+    RETURN |12-11| = 1
+    
+Example_3 ::
+
+          10
+        /   \
+      5      15
+     / \    /
+    0   7  11
+
+    RETURN |8-7| = 1
+    Notice how min diff elements lie on two seperate paths here
+    
+BST property itself imbbues much strength here on finding min distances too
+
+*/
 
 class Solution 
 {
     public int getMinimumDifference(TreeNode root) 
     {
-        if(root == null || (root.left == null && root.right == null)) return 0; // no absolute difference anyways!
-        
         
     }
-    
-    
-    
 }
-
 
 
 
