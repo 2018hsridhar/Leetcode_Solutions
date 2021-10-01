da492d2de9118ff5bfd8bf90e69509af0a730a30
Hari Sridhar <hari.h.sridhar@gmail.com>
2021-10-01 00:13:38 -0400

    Update leetcode_97.java

--- leetcode_97.java
+++ leetcode_97.java
@@ -61,6 +61,7 @@ E.g. we know we need "aa" for "aadbbc" and not "ddbc" here!
 class Solution
 {
     boolean haveInterleave = false;
+    int[][] memo;
     public boolean isInterleave(String s1, String s2, String s3) 
     {
         haveInterleave = false;
@@ -73,10 +74,136 @@ class Solution
         if(s2.equals("") && s1.equals(s3)) return true;
         if(s1.equals("") && s2.equals("") && s3.equals("")) return true;
         
+        // [1] Initialize out MEMO matrix here
+        int m = s1.length();
+        int n = s2.length();
+        memo = new int[m+1][n+1];
+        for(int i = 0; i < m+1; ++i)
+            for(int j = 0; j < n + 1; ++j)
+                memo[i][j] = 0;
+        
         StringBuilder sb = new StringBuilder("");
-        recurse(s1,s2,s3,sb);
-        return haveInterleave;
+        // recurse(s1,s2,s3,sb);
+        // recurseTwo(s1,s2,s3);
+        return memoized(s1,s2,s3);
+        // dp(s1,s2,s3);
+
     }
+    /*
+        Memo[][] status
+        0 => unsolved
+        1 => solved : can interleave
+        2 => solved : can NOT interleave
+        Sadly with memoization, we need an extra layer of caller function scope here as well!
+    */
+
+    public boolean memoized(String s1, String s2, String s3)
+    {
+        // System.out.printf("s1 = %s \t s2 = %s \t s3 = %s\n", s1,s2,s3);
+        int m = s1.length();
+        int n = s2.length();
+        // Hey we are already there with all these cases anyways
+        // In this case, we have remainder characters : NOT a perfect interleaving!
+        if(s3.equals(""))
+        {
+            memo[m][n] = 2;
+            return false;
+        }
+        
+        if(s1.equals(""))
+        {
+            String curStr = s2;
+            if(curStr.equals(s3))
+            {
+                memo[0][s2.length()] = 1;
+                return true;
+            }
+            else
+            {
+                memo[0][s2.length()] = 2;
+                return false;                
+            }
+        }
+        if(s2.equals(""))
+        {
+            String curStr = s1;
+            if(curStr.equals(s3))
+            {
+                 memo[s1.length()][0] = 1;
+                 return true;
+            }
+            else
+            {
+                memo[s1.length()][0] = 2;
+                return false;
+            }
+        }
+        
+        char char_one = s1.charAt(0);
+        char char_two = s2.charAt(0);
+        char char_three = s3.charAt(0);
+        boolean s1_path = false;
+        boolean s2_path = false;
+        if(char_one == char_three)
+        {
+            // ideally, memo is filled by this time ( if not filled here ) 
+            if(memo[m-1][n] == 1)
+                s1_path = true;
+            if ( memo[m-1][n] == 2)
+                s1_path = false;
+            if ( memo[m-1][n] == 0)
+                s1_path = memoized(s1.substring(1), s2, s3.substring(1));
+        }
+        if(char_two == char_three)
+        {
+            // ideally, memo is filled by this time ( if not filled here ) 
+            if(memo[m][n-1] == 1)
+                s2_path = true;
+            if ( memo[m][n-1] == 2)
+                s2_path = false;
+            if ( memo[m][n-1] == 0)
+                s2_path = memoized(s1, s2.substring(1), s3.substring(1));  
+        }
+        
+        // Now store current computation, based on the other paths
+        if(s1_path || s2_path)
+            memo[m][n] = 1;
+        else
+        {
+            memo[m][n] = 2;
+            return false;
+        }
+        return true;
+    }
+    
+        
+    public void recurseTwo(String s1, String s2, String s3)
+    {
+        // System.out.printf("s1 = %s\t s2 = %s\n", s1.toString(), s2.toString());
+        if(s1.equals(""))
+        {
+            String curStr = s2;
+            if(curStr.equals(s3))
+                haveInterleave = true;
+            return;
+        }
+        if(s2.equals(""))
+        {
+            String curStr = s1;
+            if(curStr.equals(s3))
+                haveInterleave = true;
+            return;
+        }
+        
+        char char_one = s1.charAt(0);
+        char char_two = s2.charAt(0);
+        char char_three = s3.charAt(0);
+        if(char_one == char_three)
+            recurseTwo(s1.substring(1), s2, s3.substring(1));
+        if(char_two == char_three)
+            recurseTwo(s1, s2.substring(1), s3.substring(1));
+    }
+    
     
     public void recurse(String s1, String s2, String s3, StringBuilder sb)
     {
