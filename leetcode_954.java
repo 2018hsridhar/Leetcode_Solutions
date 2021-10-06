58b84f40f117c1b3cfa5771c56c99d14932564c0
Hari Sridhar <hari.h.sridhar@gmail.com>
2021-10-05 23:10:25 -0400

    Create leetcode_954.java

---
+++ leetcode_954.java
@@ -0,0 +1,34 @@
+/*
+
+Strategy -> execute a sort here
+URL = https://leetcode.com/problems/array-of-doubled-pairs/
+954. Array of Doubled Pairs
+Inputs guaranteed to be integers
+Array len reasonably bounded by [2,30K] = [2,3e5]
+Array len guaranteed even property
+Integers guaranteed reasonableness too
+
+
+Complexity : 
+Let N := len(arr)
+Time = O(N)
+Space = O(1)
+
+Edge Case Testing : 
+(A) [3,1,3,6]   => FALSE
+(B) [4,-2,2,-4] => TRUE
+(C) [-1,-2,-4,-8] => TRUE
+(D) [1,2,4,8] => TRUE
+(E) [-1,-2,-4,-8, 1, 2, 4, 8 ] => TRUE
+(F) [2,2,2] => FALSE
+
+
+*/
+
+class Solution 
+{
+    public boolean canReorderDoubled(int[] arr) 
+    {
+        
+    }
+}
