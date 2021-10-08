da75108f412b6b19e74601d6310a565e0b654403
Hari Sridhar <hari.h.sridhar@gmail.com>
2021-10-08 19:11:10 -0400

    Create leetcode_516.java

---
+++ leetcode_516.java
@@ -0,0 +1,45 @@
+/*
+
+String s is guaranteed at least [1,1000] characters and is composed only of [a-z] lowercase English letters
+Find the length of the longest palindromic subsequence in string s
+
+516. Longest Palindromic Subsequence
+URL = https://leetcode.com/problems/longest-palindromic-subsequence/
+
+Recursive Complexity : 
+Let N := len(string s)
+Time = 
+Space = 
+
+DP Complexity : 
+Time = 
+Space = 
+
+Edge Case testing : 
+(A) s="b"
+    => 1
+(B) s="abc"
+    =>1
+(C) s="bb"
+    =>2
+(D) s="abba"
+    =>4
+(E) s="abcdefghijkl"
+    => 0
+(F) s="abbacdef"
+    => 4
+(G) s="cdefcabba"
+    => 4
+(H) s="abbacdeedc"
+    => 6
+(I) s="abcdeffedcba"
+    => 12
+
+
+*/
+
+class Solution {
+    public int longestPalindromeSubseq(String s) {
+        
+    }
+}
