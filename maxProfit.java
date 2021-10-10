5ee383bec44dbd967312cbc3747f5348e94ea0fc
Hari Sridhar <hari.h.sridhar@gmail.com>
2021-10-10 19:54:55 -0400

    Create maxProfit.java

---
+++ maxProfit.java
@@ -0,0 +1,20 @@
+/*
+    Have a set of days corresponding to their prices of given stocks
+    Can hold @ most one share of the stock at any time
+    Can buy it then sell it on the same day ( but this nets a profit of zero anyways ) 
+    Return max profit possible
+    
+    URL = https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
+    122. Best Time to Buy and Sell Stock II
+
+
+    
+*/
+
+class Solution 
+{
+    public int maxProfit(int[] prices) 
+    {
+        
+    }
+}
