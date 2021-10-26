04e2dae701661c602df67153c31868ed643bcf4f
Hari Sridhar <hari.h.sridhar@gmail.com>
2021-10-26 14:57:12 -0400

    Update leetcode_79.java

--- leetcode_79.java
+++ leetcode_79.java
@@ -1,115 +1,136 @@
 /*
-THOUGHT PROCESS : 
-- First establish that the first character of the word exists in this board
-- Then perform depth search and see if one can find the letter in this board ( 4 cardinal directions )  [ recursive in nature ] 
-- Akin to the Game "Connect 4"
-- Do not use same cell more than once
-- Yes strings can have repeating characters
 
-Ideal computational complexity : Time = O(4^N), Space = O(1) where N = number of characters in word
-WE know m,n are reasonable here [ boudned by the closed interval of integers {1,6}]
-Word length also reasonable [ bounded by len = 15 ] 
-Range of letters reasonable - only lowercase/uppercase here
+79. Word Search
+URL = https://leetcode.com/problems/word-search/
 
-In recursive method : pass in the index of existing depth
-Akin to flood-fill/level-order BFS solutions
+ 2D array ( matrix ) where w,h can be unequal, containing letters
+ Given a list of words
+ Return an List of all words contained in the board
+ Words need NOT be in ANY order
+ Connect adjacent ( hoz,vert,diag) letters - do not use a letter more than once though at a given position
+ Words can have duplicate letters, BUT, they must originate from elsewhere in said board
+ >= 2 words can overlap and use the same letters
+ Attempt @ confusion : general thinking is the words are [a-z]. they can include numbers and other symbols
+ 	-> can not always mark entries in the board with a symbol for own visition ( e.g. X or -1 ) 
+	-> hence, you need a seperate board object ( boolean for ease ) 
 
-Board is sufficiently tiny, so a O(MN) lookup for first letter is not unreasonable performance here :-)
-Use Character.MIN_VALUE / empty character literal trickto prevent searching over same block again
-In many backtracking algorithms, lists of current candidates/state space need to be passed in as a global parameter
+(4x4)
+TEST BENCH 
+(A) [[]]
+(B) [["t","h","i","s"], ["o","a","o","a"],["t","m","t","h"],["e","b","a","d"]]
+	["this", "that", "tame", "iota", "some", "tote"]
+	["iota", "some", "tame", "that", "this", "tote"]
+(C) [["t","h","i","s"], ["o","a","o","a"],["t","m","t","h"],["e","b","a","d"]]
+	["thid", "thpt", "sand"]
+	[]
+(D) [["t","h","i","s"], ["o","a","o","a"],["t","m","t","h"],["e","b","a","d"]]
+	 ["song","slme"]
+	 []
+ (E) [["a", "b", "c", "c"]]
+ 	["a","ab","bcd","abcd"]
+(F)
+ [
+  ["t", "h", "i", "s", "i", "s", "a"],
+  ["s", "i", "m", "p", "l", "e", "x"],
+  ["b", "x", "x", "x", "x", "e", "b"],
+  ["x", "o", "g", "g", "l", "x", "o"],
+  ["x", "x", "x", "D", "T", "r", "a"],
+  ["R", "E", "P", "E", "A", "d", "x"],
+  ["x", "x", "x", "x", "x", "x", "x"],
+  ["N", "O", "T", "R", "E", "-", "P"],
+  ["x", "x", "D", "E", "T", "A", "E"]
+]
+["this", "is", "not", "a", "simple", "boggle", "board", "test", "REPEATED", "NOTRE-PEATED"]
 
-Careful here : BFS/DFS != typical backtracking. Backtracking is more index built - it's not a list of candidates at each level
-Your skeleton here will tank you boyo!
+Strategies : recursion, DFS, flood-fill
+Start of each word / first character = thy exploration root
+Can test each word independently ( you may have to actually )
+Can we sort the input to test alphabets as well ( a hash, in a sense )?
 
+Recall that guy's global visited technique as well too on the callback
 
-Edge case testing : 
+COMPLEXITY
+Let M,N := dimensions of board
+Let H := length longest string in words
+Let W := number of words in the input
+Assuming that the input is NOT sorted
+TIME = O(W*(2*MN)) = O(WMN)
+	start = O(MN), expl = O(MN)
+SPACE = O(H) + O(MN) + O(H) = O(MN) + O(H)
 
+Modularization makes reasoning of Big-O easier as well
+* Clement's solution utilizes a trie underneath, and therefore, their TS reasoning will differ substantially
+
+is the algo expert boggle board problem, but NOT played diagonally
 
 */
 class Solution 
 {
-    public boolean exist(char[][] board, String word) 
-    {
-        // [1] Check if first character of word even exists on the board. Make sure to add all candidates which can be the first character too!
-        ArrayList<int[]> candidates = new ArrayList<int[]>();
-        char firstLet = word.charAt(0);
-        int m = board.length;
-        int n = board[0].length;
-        boolean foundFirstLet = false;
-        int[] firstPos = new int[2];
+	// Try to abstract away data structure conversions : may prove confusing
+    public boolean exist(char[][] board, String word)
+	{
+		return playBoggle(board,word);
+	}
+	
+	// Seperation : Discovery of start position from the actual dfs call
+	private boolean playBoggle(char[][] board, String token)
+	{
+		int m = board.length;
+		int n = board[0].length;
+		int[][] visited = new int[m][n]; // because IDK if bools initialize to false or true, but ints always 0-init
         for(int i = 0; i < m; ++i)
         {
             for(int j = 0; j < n; ++j)
             {
-                if(board[i][j] == firstLet)
-                {
-                    foundFirstLet = true;
-                    candidates.add(new int[]{i,j});
-                }
+                    if(dfs(board,visited,token,0,i,j) == true)
+                    {
+                        return true;
+                    }
             }
         }
-        if(foundFirstLet = false)
-            return false;
-        
-        // [2] now recurse 
-        int depth = 0;
-        return backtrack(board, word, depth, candidates);
-    }
-    
-    public boolean backtrack(char[][] board, String word, int depth, ArrayList<int[]> candidates)
-    {
-        // [1] Check if already at maximum possile depth allowed ( assuming obtained fully positive string )
-        if(depth == word.length())
-            return false; // passed our boundary now!
-        
-        // [2] Check current candidates set now and FLAG them to not be visited again!
-        ArrayList<int[]> nextLevel = new ArrayList<int[]>();
-        for(int i = 0; i < candidates.size(); ++i)
-        {
-            int[] curCand = candidates.get(i);
-            int x = curCand[0];
-            int y = curCand[1];
-            char myLet = board[x][y];
-            System.out.printf("At (%d,%d) let = %s depth = %d\n", x,y,myLet, depth);
-            if(word.charAt(depth) == myLet && depth == word.length() - 1)
-                return true;
-            
-            if(word.charAt(depth) == myLet)
-            {
-                board[x][y] = Character.MIN_VALUE;
-          
-                // [3] Perform bounds check on other candidates and add them to new list
-
-                // West
-                if((x-1) >= 0 && board[x-1][y] != Character.MIN_VALUE)
-                    nextLevel.add(new int[]{x-1,y});
-
-                // East
-                 if((x+1) < board.length && board[x+1][y] != Character.MIN_VALUE)
-                    nextLevel.add(new int[]{x+1,y});
-
-                // North
-                if( (y-1) >= 0 && board[x][y-1] != Character.MIN_VALUE)
-                    nextLevel.add(new int[]{x,y-1});
-
-                // South
-                 if((y+1) < board[0].length && board[x][y+1] != Character.MIN_VALUE)
-                    nextLevel.add(new int[]{x,y+1});
-            }
-        }
-        
-        return backtrack(board, word, ++depth, nextLevel); // short eval here going on! OMG ( helper || foundEval )
-    }
+		return false;
+  }
+	
+	// Return of the data type may trip up folks here
+	private boolean dfs(char[][] board, int[][] visited, String word, int idx, int i, int j)
+	{
+		int m = board.length;
+		int n = board[0].length;
+		if(i < 0 || i >= m || j < 0 || j >= n)
+		{
+			return false;
+		}
+		if(visited[i][j] == 1)
+		{
+			return false;
+		}
+		// base case handling : single len string
+		if(word.charAt(idx) == board[i][j])
+		{
+			if(word.length()-1 == idx)	// Need to now search like this
+			{
+				return true;
+			}
+			else
+			{
+				visited[i][j] = 1;
+				// Need the diagonals now too
+				boolean toTop = dfs(board, visited, word, idx+1, i+1, j);
+				boolean toBottom = dfs(board, visited, word, idx+1, i-1, j);
+				boolean toLeft = dfs(board, visited, word, idx+1, i, j-1);
+				boolean toRight = dfs(board, visited, word, idx+1, i, j+1);
+				
+				visited[i][j] = 0;
+				boolean cardinals = (toTop || toBottom || toLeft || toRight);
+				boolean res = (cardinals); 
+				return res;
+			}
+		}
+		else
+		{
+			return false;
+		}
+	}
+	
+	
 }
-
-
-
-
-
-
-
-
-
-
-
-
