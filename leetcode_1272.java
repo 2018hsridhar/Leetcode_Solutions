b2f5e94dc6c8fc1303957e6bb9e879b7a968ba93
Hari Sridhar <hari.h.sridhar@gmail.com>
2021-11-03 23:38:14 -0400

    Create leetcode_1272.java

---
+++ leetcode_1272.java
@@ -0,0 +1,25 @@
+/*
+
+1272. Remove Interval
+URL = https://leetcode.com/problems/remove-interval/
+
+Note : your input list is sorted anyways, so the output list will still be sorted here
+
+COMPLEXITY : 
+
+TEST BENCH : 
+(A)
+(B)
+(C)
+(D)
+
+
+
+*/
+class Solution 
+{
+    public List<List<Integer>> removeInterval(int[][] intervals, int[] toBeRemoved) 
+    {
+        
+    }
+}
