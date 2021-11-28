b7bfa02d2ab9a349297382da8a433b7d4bc2f2ba
Hari Sridhar <hari.h.sridhar@gmail.com>
2021-11-27 20:02:57 -0500

    Update leetcode_1982.java

--- leetcode_1982.java
+++ leetcode_1982.java
@@ -3,6 +3,12 @@
 1982. Find Array Given Subset Sums
 URL = https://leetcode.com/problems/find-array-given-subset-sums/
 
+CLARIFICATION
+1. Can not make any set with n = 0 : a terminating case
+2. No null inputs or empty inputs as well
+3.
+
+
 COMPLEXITY
 
 TEST CASES
@@ -16,7 +22,9 @@ TEST CASES
     [-6, -5, -4]
 (E) n = 3, sums = [1,2,3,4,5,6]
     [1,2,3]
-(F)
+( *** EASIEST TEST CASE *** ) 
+(F) n = 3, sums = [1,2,3,4,5,6]
+    [1,2,3]
 (G)
 (H)
 (I)
@@ -26,7 +34,8 @@ W.R.T. memory and time as well
 The addition of the sums should NOT cause SIGNED INTEGER data overflow though
 Luckily, the hashmap comparison is limited as well ( 2^15 max subset sums here ) 
 -> potential issue : hashmap itself may be emptied out, but we may have remainder elements as well
-
+-> Indeed true that subsets is the similar question here :-)
+-> note : must return final subset : we still need the subset sums -> 2 objects in the backtracking?
 
 PSEUDOCODE : 
 
@@ -40,13 +49,19 @@ PSEUDOCODE :
     // Why return a boolean? Perhaps an object could be more sensical too? 
     // We do create those child lists . Just convert ArrayList or HM to Array Later on
     // Null object comparison test ( objects are pointers : NOT data ) 
-    public ArrayList<Integer> backtrack(int[] sums, int n, int idx, List<Integer> ArrayList)
+    // Break point : either meet <n> or meet end of iteration with <j> here
+    private ArrayList<Integer> backtrack(int[] sums, int n, int idx, List<Integer> parentSubset, List<Integer> parentSubsetSums)
     {
+        if(n == 0)
+        {
+            return null;
+        }
+        
         boolean foundValidSubset = false;
         int j = i + 1;
         while(j < n)
         {
-            boolean childRes = backtrack(sums, n, j, childList);
+            ArrayList<Integer> childList = backtrack(sums, n - 1, j, __, __);
             if(childRes != null)
             {
                 foundValidSubset = true;
@@ -55,9 +70,26 @@ PSEUDOCODE :
         }
         return null;
     }
+    
     // [2] The results array
-    
-    
+    // OBSERVE that the seperation of functinos further assists in call stack debugging too.
+    public int[] recoverArray(int n, int[] sums)
+    {
+        if(n == 0 || sums.length == 0)
+            return new int[]{};
+        List<Integer> parentSubset = new ArrayList<Integer>();
+        List<Integer> parentSubsetSums = new ArrayList<Integer>();
+        List<Integer> validSet = backtrack(sums, n, 0, parentSubset, parentSubsetSums);
+        if(validSet == null)
+            return new int[]{};
+        int sz = validSet.size();
+        int[] recover = new int[sz];
+        for(int i = 0; i < validSet.size(); ++i)
+        {
+            recover[i] = validSet.get(i);
+        }
+        return recover;
+    }    
     
 
 */
