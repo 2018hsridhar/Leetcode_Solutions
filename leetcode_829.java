c3ad345b990f43079d55d6e55c5aaa0ca6c328cf
Hari Sridhar <hari.h.sridhar@gmail.com>
2021-11-29 00:04:46 -0500

    Create leetcode_829.java

---
+++ leetcode_829.java
@@ -0,0 +1,30 @@
+/*
+
+URL = https://leetcode.com/problems/consecutive-numbers-sum/
+829. Consecutive Numbers Sum
+
+COMPLEXITY
+Let N := the number provided
+TIME = ___
+SPACE = ___ ( explicit ) ___ ( implicit ) 
+
+TEST CASES
+(A) n = 1
+(B) n = 5
+(C) n = 9
+(D) n = 14
+(E) 
+(F) n = 10e3 = 1,000
+(G) n = 10e6 = 1,000,000
+(H) n = 10e8 = 100,000,000
+(I) n = 10e9 = 1,000,000,000
+
+Test power in series : we note how they get stopped as well too
+*/
+class Solution 
+{
+    public int consecutiveNumbersSum(int n) 
+    {
+        
+    }
+}
