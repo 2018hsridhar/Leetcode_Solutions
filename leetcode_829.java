ab8abd47821cbc5429bcdad3aba6739f2869140d
Hari Sridhar <hari.h.sridhar@gmail.com>
2021-11-29 00:08:19 -0500

    Update leetcode_829.java

--- leetcode_829.java
+++ leetcode_829.java
@@ -10,7 +10,15 @@ SPACE = ___ ( explicit ) ___ ( implicit )
 
 TEST CASES
 (A) n = 1
+    =>1
+(B) n = 2
+    =>1
+(C) n = 3
+    =>2
 (B) n = 5
+    =>2
+(C) n = 4
+    => 0
 (C) n = 9
 (D) n = 14
 (E) 
@@ -20,6 +28,13 @@ TEST CASES
 (I) n = 10e9 = 1,000,000,000
 
 Test power in series : we note how they get stopped as well too
+10e9 possible integer elmeents in a contiguous array?
+
+Strategies : Recursion, DP, Iteration, Combinations
+
+
+
+
 */
 class Solution 
 {
