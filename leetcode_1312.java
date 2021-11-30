6c73632c6e33b45fb4e2513b0ff3f1895b839e6a
Hari Sridhar <hari.h.sridhar@gmail.com>
2021-11-30 10:53:10 -0500

    Create leetcode_1312.java

---
+++ leetcode_1312.java
@@ -0,0 +1,31 @@
+/*
+
+1312. Minimum Insertion Steps to Make a String Palindrome
+URL = https://leetcode.com/problems/minimum-insertion-steps-to-make-a-string-palindrome/
+
+Character literal insertions can be performed at any index of the string variable
+
+
+Strategies : Greedy ( Recursive, but is Minimal ) or DP, Iterative
+
+COMPLEXITY
+Let N :=  #-characters in the string
+TIME = O() 
+SPACE = O() ( EXPLICIT ) O() ( IMPLICIT ) 
+
+TEST CASES : 
+(A)
+(B)
+(C)
+(D)
+(E)
+
+
+*/
+class Solution 
+{
+    public int minInsertions(String s) 
+    {
+        
+    }
+}
