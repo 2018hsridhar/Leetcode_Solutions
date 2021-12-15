3a6f0a7ec0ea84db76a32d902ea1f1471fc54f3f
Hari Sridhar <hari.h.sridhar@gmail.com>
2021-12-14 22:24:08 -0800

    Update leetcode_7.java

--- leetcode_7.java
+++ leetcode_7.java
@@ -16,12 +16,25 @@ Use the ceil of the log function of CEIL[log_10(x)] to get number digits with ex
 
 
 TEST CASES : 
-(A)
-(B)
-(C)
-(D)
-(E)
-(F)
+(A) -10000
+    => -1
+(B) -INT_MIN -2147483648 
+    => fails
+(C) INT_MAX =2147483647
+    => succeeds
+(D) 0,1, -1
+    => base case handling
+(E) 70023
+    => sure, mod (1000,100) leaves the dangling "0023" for a whilst.
+    => eventually resolves later though
+(F) 121
+    => Check iteration bug too
+(G) 2147483647
+    7463847412 = rev(INT_MAX) by digits : > INT_MAX
+     746384741 = rev(INT_MAX) by digits with suffix = 2 dropped off
+(H)
+(I)
+(J)
 
 */
 class Solution 
@@ -29,21 +42,56 @@ class Solution
     public int reverse(int x) 
     {
         // Special range handling due to ceil(log(x)) handling
-        if(-1 <= x || x <= 1)
+        if(-1 <= x && x <= 1 )
         {
+            System.out.printf("here\n");
             return x;
         }
-        int numDigits = (int)Math.ceil(Math.log10(x));
-        System.out.printf("Num digits for [%d] x = [%d]\n", x, numDigits);
         boolean out32BitRange = false;
         boolean isNeg = false;
         if(x < 0)
         {
             isNeg = true;
-            // x *= -1;    Can lead to issue if X = INT_MIN : careful here!
+            if(x == Integer.MIN_VALUE)
+            {
+                return 0;
+            }
+            // Can lead to issue if X = INT_MIN : careful here!
+            x *= -1; 
         }
+        // Heads up : logarithms fail on NEGATIVE inputs. Must be positive inputs strictly!
+        int cur = x;
+        int numDigits = (int)Math.floor(Math.log10(x)) + 1;  // Shit : not fully right here -> exert caution again! -> is a floor function : NOT a ceil function
+        // System.out.printf("Num digits for [%d] x = [%d]\n", x, numDigits);
+        int sigDig = numDigits - 1; // So for 123, sigDig = 2 then. Offset for 0-indexing of digit powers
+        int pow = 0;
         int res = 0;
-        
+        int maxAdd = 2 * (int)Math.pow(10,9);
+        while(cur >= 1)  // Check if eq to 1 or 0 here -> careful
+        {
+            int divisor = (int)Math.pow(10, sigDig);
+            int digit = (int) cur / divisor;
+            // System.out.printf("Digit = %d\n", digit);
+            int toAdd = (int)Math.pow(10,pow) * digit;
+            int diff = Integer.MAX_VALUE - res;
+            // System.out.printf("Res = %d\n", res);
+            if((pow >= 9 && digit >= 3) || toAdd > diff) // Order of eval in circuit evaluations matters a lot
+            {
+                return 0;
+            }
+            else
+            {
+                res += toAdd;
+                // System.out.printf("Res = %d\n", res);
+            }
+            cur = cur % divisor;
+            sigDig--;
+            pow++;
+        }
+        if(isNeg) 
+        {
+            res *= -1;
+        }
         return res;
     }
 }
