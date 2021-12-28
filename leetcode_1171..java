17199eece9a0b01b0d30f040d56ec76f9de42996
Hari Sridhar <hari.h.sridhar@gmail.com>
2021-12-28 17:35:58 -0500

    Update leetcode_1171..java

--- leetcode_1171..java
+++ leetcode_1171..java
@@ -10,48 +10,76 @@
  */
 
 /*
-  THOUGHT PROCESSES
-  
-  IDEAL : [T,S] = [O(N), O(1)] , where N = number of elements in the LL
-  Node sequences must be consecutive in nature
-  
-  Notice : most LL inputs/outputs tend to be their corresponding serializations of objects. Hence why such questions of serialization/deserialization are asked in interviews.
-  
-  Ma length LL is 1000. Bounded by the closed interval [1,1000]
-  Values of nodes are bounded by the closed interval [-1000, 1000]
-  
-  We can obtain the sum of node values in O(n) time and try to construct the resulting new LL backwards?
-  If sum = 0 : we are effectively done
-  
-  Suppose every values where positive, we'd know that summation too
-  
-  Build up the cases too
-  [1] -> [1]
-  [1, 2] -> [1, 2]
-  [1, 2, 3] -> [1, 2, 3 ]
-  [1, 2, -3 ] -> []
-  [1,2,-4] -> [1,2,-4]
-  
-  SPENT 30 MINUTES TRYING TO INITIALLY SOLVE!
-  
-  Is there a sliding window technique here?
-  
-  And suppose input were sorted too? Could this help with understanding?
-  Can a running sum also be stored here?
-  
-  Does sorting really help here? Values in the LL must be consecutive too!
-  
-  BASE CASE : n = 1 element - just return list itself
-  
-  EDGE CASES : 
-  [1,2,-4] -> [1,2,-4] since the sum of all elements = -1; not 0
-  
-  Problem seems very "stack-like" in nature : push and pop elements and see whether your current running sum = 0 or not
-  
-  
- */
-class Solution {
-    public ListNode removeZeroSumSublists(ListNode head) {
-        return null;
+
+CLARIFICATIONS :
+
+Given head of a SLL
+We repeatedly delete consecutive node sequences
+Return head of the final SLL ( head may change if prefix of the list yields zero )
+ListNode objects contain INTEGRAL values ( they may skew negative or zero as well, and skew duplicates, but they are in range [-1K, 1K] )
+Always ask -> if many answers, or only one here, is acceptable.
+
+URL = https://leetcode.com/problems/remove-zero-sum-consecutive-nodes-from-linked-list/
+1171. Remove Zero Sum Consecutive Nodes from Linked List
+
+THOUGHT PROCESSES : 
+1. Can incorporate use of a hashmap here ( running sums -> node which led to that summation )
+2. Can also utilize 
+3. Two pointers technique
+
+
+COMPLEXITY
+Let N := #-nodes in the SLL
+Let K := length longest zero sum sequence in the SLL
+TIME = O()
+SPACE = O() ( EXP ) O() ( IMP )
+
+TEST CASES : 
+(A) [0,0,0,0,0,0]
+    [] <- head points to NULL here
+(B) [0,0,0,0,0,1]
+    [1] <- head points to final node
+(C) [1,5,2,6,2,3,9]
+    [1,5,2,6,2,3,9] <- head points to start @ 1 ( no deletions took place )
+(D) [-1,-2,-3,-4,-5,15]
+    [] <- head points to NULL ( all elems deleted )
+(E) [1,6,-5,4,-3,2,-1,10]
+    [1,6,-5,4,-3,2,-1,10] <- head points to first el ( no deletions at all )
+(F) [1,2,3,-6,10]
+    [10] <- head points to 10 ( delete the prefix ) 
+(G) [10, 1,2,3,-6]
+    [10] <- suffix deletion
+(H) [1,2,3,-3,-3,1,2]  
+    [1,2] <- head of new SLL ( either the LHS or the RHS )
+(I)
+(J)
+
+
+PSEUDOCODE :
+
+    HashMap<run_sum,node> HM = ()
+    runSum = 0
+    ptr = head
+    while(ptr != null)
+        runSum += ptr.val
+        if(HM contains key runSum)
+        {
+        
+        }
+        else
+        {
+            HM put tuple (runSum, ptr)
+        }
+        ptr = ptr.next
+    
+    
+
+*/
+
+class Solution 
+{
+    public ListNode removeZeroSumSublists(ListNode head) 
+    {
+        
     }
 }
