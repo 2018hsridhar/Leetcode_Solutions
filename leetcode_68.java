d3a498c117a9b7b8db1205d550fc6199d5e00d35
Hari Sridhar <hari.h.sridhar@gmail.com>
2022-02-03 21:27:20 -0500

    Update leetcode_68.java

--- leetcode_68.java
+++ leetcode_68.java
@@ -19,11 +19,31 @@ Constraints
 - words only English letters and Englih symbols
 
 TEST CASES
-(A)
-(B)
-(C)
-(D)
-(E)
+(A)  ["enough","to","explain","to"]
+20
+    Fully perfect string here
+    => PASS
+(B) ["enough","to","explain","tooth"]
+20
+    => PASS
+(C) ["enough","to","explain","abcdefghijklmnopqrst","tooth"]
+20
+    => Div by 0 ( cuz one word only in "abcd...rst" )
+    => PASS
+(D) ["enough","to","explain","abcdefghijklmnopqrst","abcdefghijklmnopqr", "abcdefghijklmnopqrst"]
+20
+    => knew this case would not work as epected ... resolve!
+    => PASS
+(E) ["This", "is", "an"] 20
+    => PASS
+(F) ["a","b","c","d"]
+1
+=> PASS
+(G) ["abc","defghi","jk","lm","nop","qr","stuvwxyz"]
+10
+    => PASS
+(H)
+
 
 Strategies : Counting, Enueration, StringBuilder logic
 
@@ -36,6 +56,8 @@ Strategies : Counting, Enueration, StringBuilder logic
 Work off assumption : one space gap @ minimum between each word
 Goal : can we chop down repeated code in ANy of these code paths as well?
 
+Key gotcha : the rightmost never has spaces added to it! Take stock of this fact!
+
 */
 class Solution {
     public List<String> fullJustify(String[] words, int maxWidth) 
@@ -44,7 +66,8 @@ class Solution {
         int w = words.length;
         int l = 0;
         int r = 0;
-        int remLen = maxWith;   // reset counter mechanism here as well
+        int remLen = maxWidth;   // reset counter mechanism here as well
+        int numWhiteSpaces = 0;
         // How to code up for bottom-row case as well here?
         String space = " ";
         while(r < w)
@@ -61,41 +84,112 @@ class Solution {
             }
             else if(remLen == curLen)
             {
-                remLen -= curLen;
-                
-                
-                
-                
+                int numWordsInClause = (r - l + 1);
+                StringBuilder  clause = new StringBuilder ("");
+                if(numWordsInClause == 1)
+                {
+                    clause.append(words[l]);
+                    remLen = maxWidth;
+                    remLen -= words[l].length();
+                    for(int k = 0; k < remLen; ++k)
+                        clause.append(space);
+                }
+                else
+                {
+                    int numGaps = numWordsInClause - 1;
+                    int gapSize = numWhiteSpaces / numGaps;
+                    int gapAdd = numWhiteSpaces % numGaps;  // keep adding this
+
+                    while(l <= r)
+                    {
+                        clause.append(words[l]);
+                        if(l < r)   // Notice this condition as well!
+                        {
+                            for(int i = 0; i < gapSize; ++i)
+                                clause.append(space);
+                            if(gapAdd > 0)
+                            {
+                                clause.append(space); 
+                                gapAdd -= 1;
+                            }
+                        }
+                        ++l;
+                    }
+                }
+                justified.add(clause.toString());
+    
+                // RESET pointers and counters
+                remLen = maxWidth;
                 ++r;
+                l = r;
+                numWhiteSpaces = 0;
             }
             else    // can not append next word to the right of text
             {
                 // Given two pointers, close them in and add spaces, based on parity
                 numWhiteSpaces += remLen;   // in our case, 4 words => 3 gaps, but 5 whitespaces
                 int numWordsInClause = (r - l);
-                int numGaps = numWordsInClause - 1;
-                int gapSize = numWhiteSpaces / numGaps;
-                int gapAdd = numWhiteSpaces % numGaps;  // keep adding this
-                
-                StringBuidler clause = new StringBuilder("");
-                while(l < r)
+                StringBuilder  clause = new StringBuilder ("");
+                if(numWordsInClause == 1)
                 {
                     clause.append(words[l]);
-                    for(int i : gapSize)
+                    remLen = maxWidth;
+                    remLen -= words[l].length();
+                    for(int k = 0; k < remLen; ++k)
                         clause.append(space);
-                    if(gapAdd > 0)
-                    {
-                        clause.append(space); 
-                        gapAdd -= 1;
-                    }
-                    ++l;
                 }
+                else
+                {
+                    int numGaps = numWordsInClause - 1;
+                    int gapSize = numWhiteSpaces / numGaps;
+                    int gapAdd = numWhiteSpaces % numGaps;  // keep adding this
+
+                    while(l < r)
+                    {
+                        clause.append(words[l]);
+                        if(l < r-1)
+                        {
+                            for(int i = 0; i < gapSize; ++i)
+                                clause.append(space);
+                            if(gapAdd > 0)
+                            {
+                                clause.append(space); 
+                                gapAdd -= 1;
+                            }
+                        }
+                        ++l;
+                    }
+                }
+                justified.add(clause.toString());
                 
                 // Reset counters AND pointers
-                l = r;  // start the pointers again
+                l = r;  // start the pointers again : also not : do NOT increment r, as we have to restart
                 remLen = maxWidth;
+                numWhiteSpaces = 0;
             }
         }
+        // Last case -> must fill out fully here, if remLen > 0 ( last sentence ) 
+        StringBuilder  clause = new StringBuilder ("");
+        int curLen = 0;
+        if(l < r)
+        {
+            while(l < r)
+            {
+                clause.append(words[l]);
+                clause.append(space);
+                curLen += words[l].length();
+                curLen += 1;
+                l += 1;
+            }
+            while(curLen < maxWidth)
+            {
+                clause.append(space);
+                curLen += 1;
+            }
+            justified.add(clause.toString());
+        }
+
+        
         return justified;
     }
 }
