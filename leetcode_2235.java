7eb8a546a2bcd19dd4f7d9e45990277297114137
Hari Sridhar <hari.h.sridhar@gmail.com>
2022-09-20 12:36:19 -0700

    Create leetcode_2235.java

---
+++ leetcode_2235.java
@@ -0,0 +1,10 @@
+/*
+2235. Add Two Integers
+URL = https://leetcode.com/problems/add-two-integers/
+*/
+
+class Solution {
+    public int sum(int num1, int num2) {
+        return num1 + num2;   
+    }
+}
