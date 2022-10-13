1087355b9c0d304751a5d8afbacccccd2c2f574c
Hari Sridhar <hari.h.sridhar@gmail.com>
2022-10-12 20:47:33 -0700

    Create leetcode_595.sql

---
+++ leetcode_595.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+# 595. Big Countries
+# URL = https://leetcode.com/problems/big-countries/
+SELECT name, population, area FROM World
+WHERE ( population >= 25000000 OR area >= 3000000 )
