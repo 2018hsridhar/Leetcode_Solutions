87302079d00e928aef06b32f26663a62bc744c07
Hari Sridhar <hari.h.sridhar@gmail.com>
2022-11-09 20:22:44 -0800

    Update leetcode_723.cpp

--- leetcode_723.cpp
+++ leetcode_723.cpp
@@ -1,6 +1,6 @@
 /*
-URL = https://leetcode.com/problems/candy-crush/
 723. Candy Crush
+URL = https://leetcode.com/problems/candy-crush/description/
 
 */
 class Solution {
@@ -10,6 +10,7 @@ public:
         while(true){
             // [1] check sequences to destroy
             bool toExecuteCrush = inspectBoard(board);
+            // printf("CRUSH\n"); // why 3 crushes!
             if(!toExecuteCrush){
                 break;
             } else {
@@ -28,12 +29,12 @@ private:
             int rIdx = m-1;
             int wIdx = m-1;
             while(rIdx >= 0 && wIdx >= 0){
-                if(board.at(wIdx).at(j) == -1){
-                    wIdx++;
+                if(board.at(wIdx).at(j) < 0){
+                    wIdx--;
                 } else {
                     board.at(rIdx).at(j) = board.at(wIdx).at(j);
-                    rIdx++;
-                    wIdx++;
+                    rIdx--;
+                    wIdx--;
                 }
             }
             while(rIdx >= 0){
@@ -43,18 +44,19 @@ private:
         }
     }
 
-    bool inspectBoard(vector<vector<int>>& board){
-        const map<int,vector<vector<int>>> offsets;
+    bool inspectBoard(vector<vector<int>>& grid){
+        // Refactor to standalone method for maps initialization.
+        map<int,vector<vector<int>>> offsets;
         offsets[0] = {{1,0},{2,0}};
         offsets[1] = {{-1,0},{-2,0}};
         offsets[2] = {{0,1},{0,2}};
         offsets[3] = {{0,-1},{0,-2}};
         bool haveCandyToCrush = false;
-        int m = board.size();
-        int n = board[0].size();
+        int m = grid.size();
+        int n = grid[0].size();
         for(int r = 0; r < m; ++r){
             for(int c = 0; c < n; ++c){
-                int el = board.at(r).at(c);
+                int el = abs(grid.at(r).at(c));
                 if(el != 0){
                     // 4 cardinal dir check
                     for(int i = 0; i < 4; ++i){
@@ -62,11 +64,21 @@ private:
                         int nextCFirst = c + offsets[i].at(0).at(1);
                         int nextRSecond = r + offsets[i].at(1).at(0);
                         int nextCSecond = c + offsets[i].at(1).at(1);
-                        if(isInBounds(nextRFirst,nextCFirst) && isInBounds(nextRSecond,nextCSecond)){
-                            if(grid.at(nextRFirst).at(nextCFirst) == el && grid.at(nextRSecond).at(nextCSecond) == el){
+                        if(isInBounds(nextRFirst,nextCFirst,grid) && isInBounds(nextRSecond,nextCSecond, grid)){
+                            if(abs(grid.at(nextRFirst).at(nextCFirst)) == el && abs(grid.at(nextRSecond).at(nextCSecond)) == el){
+                                if(grid.at(r).at(c) > 0){
+                                    grid.at(r).at(c) *= -1;
+                                    // printf("Crush indices [%d,%d]\n", r,c);
+                                }
                                 // mark position ( via negation :-) )
-                                grid.at(nextRFirst).at(nextCFirst) *= -1;
-                                grid.at(nextRSecond).at(nextCSecond) *= -1;
+                                if(grid.at(nextRFirst).at(nextCFirst) > 0) {
+                                    grid.at(nextRFirst).at(nextCFirst) *= -1;
+                                    // printf("Crush indices [%d,%d]\n", nextRFirst, nextCFirst);
+                                }
+                                if(grid.at(nextRSecond).at(nextCSecond) > 0){
+                                    grid.at(nextRSecond).at(nextCSecond) *= -1;
+                                    // printf("Crush indices [%d,%d]\n", nextRSecond, nextCSecond);
+                                }
                                 haveCandyToCrush = true;
                             }
                         }
@@ -74,6 +86,7 @@ private:
                 }
             }
         }
+        // printf("-----------------\n");
         return haveCandyToCrush;
     }
 
