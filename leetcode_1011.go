64ca963ffd3a43478534a6cdb3de3b056d345850
Hari Sridhar <hari.h.sridhar@gmail.com>
2023-09-27 18:46:36 -0700

    Update leetcode_1011.go

--- leetcode_1011.go
+++ leetcode_1011.go
@@ -1,99 +1,66 @@
 /*
+Binary search problem in the hiding
 URL := https://leetcode.com/problems/capacity-to-ship-packages-within-d-days/
-1011. Capacity To Ship Packages Within D Days
 
-Max weightage possible := 500 * 5 * 104 = 2 * 10-3 * 10-4 = 2 * 10-7
-log2(20000000) := 25
-nlgn time is reasonable for iteration over input n times
+Let N := len(weights)
+Let W := max sum of weights possible, across all weights ( in this case, 5e2 * 5e4 = 2.5e7) = 25,000,000 ( 25 mil ) a constant
 
-Wait days is a given too ( and we must get said number exactly :-( )
-Do not change input
+Time := O(NlogW)
+Space := O(1) ( EXP & IMP ) 
 
-Maybe a false partition technique instead?
-the max weight is the sum of a given consecutive sequence OR a single element by itself
+cwc = current weight capacity
+lwc = least weight capacity
 
-DP func to return the max Weightage
-    - avoid terrible recursive combinations
-Dealing with data overflow makes these problems harder.
+Capacity returns and tests if we are within the number of days bound too!!!!
+We will always have an answer ( no case of bad ans -1 return ) 
 
-It works
-as DP
-but mem alloc issue gaaah
-// how to minimize the mem alloc for top down dp
-
-Looks like the others ran into TLE with the DP approach
-Wow -> not even DP is alwasy efficiency
-We really may have to go with binary searching too
+17 minutes
 
 */
 func shipWithinDays(weights []int, days int) int {
-    startPos := 0
-    // Forgot to put cache here for top down memo gaah
-    // ND array technique
-    // gaaah aray lengths need constness need slices
-    // cache := [len(weights)][days + 1]int{}
-    cache:= make([][]int, len(weights))       
-    for i:=0;i<len(weights);i++ {
-        cache[i] = make([]int, days + 1)
-    }
-    return numberDaysToShipWithWeight(weights,startPos,days,cache)
+   lwc := math.MaxInt32
+   low := 1
+   high := 25000000 // no comma for value assignment :-) 
+   for(low <= high) {
+       mid := low + (high-low) / 2
+       cwc := mid
+       numDaysToShip := solveNumDaysWithCapacity(weights,cwc)
+       if(numDaysToShip == -1) { // to low
+           low = mid + 1
+       } else if(numDaysToShip > days) {
+           low = mid + 1
+       } else if ( numDaysToShip <= days) {
+            if(cwc < lwc) {
+                lwc = cwc
+                high = mid - 1
+            } else if ( cwc >= lwc) {
+                low = mid + 1
+            }
+       }
+   }
+   return lwc
 }
 
-// Ensure to exhaust number of days too
-// We'd return 0, but want least weight capacity
-// Weight is never negative one though
-// use a larger data type to work with the problem?
-// {} := initialize list syntax only
-// how to pass nd cache with ease
-func numberDaysToShipWithWeight(weights []int, startPos int, days int, cache [][]int) int {
-    myBestPartitionFromStartPos := math.MaxInt
+// always have at least one day for capacity
+// note : what if case where capacity is way way to low too!
+func solveNumDaysWithCapacity(weights []int, cwc int) int {
+    numDays := 1
+    ptr := 0
     n := len(weights)
-    if (startPos >= n) || (days > 1 && startPos >= n) {
-        // It may work due to the math Min operation too?
-        // partition sizes are bounded as well
-        return math.MaxInt
-    }
-    if cache[startPos][days] > 0 {
-        return cache[startPos][days]
-    }
-    if days == 1 {
-        sum := 0
-        for i := startPos; i < n; i++ {
-            sum += weights[i]
+    runSum := 0
+    for(ptr < n) {
+        delta := weights[ptr]
+        if(delta > cwc) {
+            return -1
         }
-        return sum
-    } else if days > 1 {
-        myCurrentPartitionSize := 0
-        for i := startPos; i < n; i++ {
-            myCurrentPartitionSize += weights[i]
-            childCase := numberDaysToShipWithWeight(weights,i + 1, days - 1, cache)
-            myCurrentBestPartitionForChildCase := max(myCurrentPartitionSize, childCase)
-            // fmt.Printf("current part size /= %d \t childCase = %d\n", myCurrentPartitionSize, childCase)
-            myBestPartitionFromStartPos = min(myBestPartitionFromStartPos, myCurrentBestPartitionForChildCase)
+        if(runSum + delta > cwc) {
+            numDays += 1
+            runSum = 0
+            runSum += delta // reset here ( can not add )
+        } else {
+            runSum += delta
         }
+        ptr++
     }
-    cache[startPos][days] = myBestPartitionFromStartPos
-    return myBestPartitionFromStartPos
+    return numDays
 }
-
-// gaah one-off funcs
-func min(a, b int) int {
-    if a < b {
-        return a
-    }
-    return b
-}
-
-func max(a, b int) int {
-    if a > b {
-        return a
-    }
-    return b
-}
-
-
-
-
-
-
-
